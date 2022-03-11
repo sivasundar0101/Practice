cf1383f54ff462cd4bb907411e31a2ee6be02751
Sivabalan <sivasundar0101@gmail.com>
2022-03-11 13:43:32 +0530

    first commit

---
+++ src/sivamotor/Motorbike.java
@@ -0,0 +1,16 @@
+package sivamotor;
+
+public class Motorbike {
+	private int speed;
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}+
+	void start () {
+	System.out.println("start");	
+	
+}
+}
---
+++ src/sivamotor/motorbikerunner.java
@@ -0,0 +1,15 @@
+package sivamotor;
+
+public class motorbikerunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Motorbike ducati = new Motorbike();
+		Motorbike Tvs = new Motorbike();
+		ducati.start();
+		Tvs.start();
+		ducati.speed=100;
+
+	}
+
+}
